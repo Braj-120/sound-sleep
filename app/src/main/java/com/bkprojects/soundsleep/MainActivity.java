4b7a59f2435543ffad3ccf7b7b3df2958eeb044a
Braj K <braj.120@gmail.com>
2022-07-01 22:53:27 +0530

    3rd Commit

--- app/src/main/java/com/bkprojects/soundsleep/MainActivity.java
+++ app/src/main/java/com/bkprojects/soundsleep/MainActivity.java
@@ -1,73 +1,94 @@
 package com.bkprojects.soundsleep;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String TIME_PICKER_DIALOG_TAG = "timePicker";
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
     private TextView startTimeValue;
+    private TextView endTimeValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startTimeValue = findViewById(R.id.start_time_value);
+        endTimeValue = findViewById(R.id.end_time_value);
+        createSpinner();
+    }
+
+    /**
+     * Create the spinner for the UI
+     */
+    protected void createSpinner() {
+        List<String> modeSpinnerList = new ArrayList<>();
+        modeSpinnerList.add(this.getString(R.string.vibrate_mode));
+        modeSpinnerList.add(this.getString(R.string.silent_mode));
+        Spinner modeSpinner = findViewById(R.id.mode_select_spinner);
+        modeSpinner.setOnItemSelectedListener(this);
+        ArrayAdapter<String> modeArrayAdapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, modeSpinnerList);
+
+        modeSpinner.setAdapter(modeArrayAdapter);
+        modeSpinner.setSelection(0);
+
     }
 
     /**
      * Start the Time Selector Dialog
      * @param view View
      */
-    public void setStartTime(View view) {
-        DialogFragment startTimeFragment = new TimePickerFragment();
-        startTimeFragment.show(getSupportFragmentManager(), TIME_PICKER_DIALOG_TAG);
+    public void setTime(View view) {
+        TextView textViewToSetTemp;
+        Log.i(LOG_TAG, view.getId() + " "+ R.id.select_start_time);
+        if (view.getId() == R.id.select_start_time) {
+            textViewToSetTemp = startTimeValue;
+        } else if(view.getId() == R.id.select_end_time) {
+            textViewToSetTemp = endTimeValue;
+        } else {
+            throw new IllegalStateException("Unexpected value: " + view.getId());
+        }
+
+        final TextView textViewToSet = textViewToSetTemp;
+        TimePickerUtil tmUtil = new TimePickerUtil(this);
+        tmUtil.setTimeListener((view1, time) -> processTimePickerResult(textViewToSet, time));
+        tmUtil.show();
+
     }
 
-    /**
-     * Converts the out time of date picker to a readable time
-     * @param hour Hour
-     * @param minute Minute
-     * @return String
-     */
-    public String getTimeIn12Hours(int hour, int minute) {
-        String am_pm = "AM";
-        String s_minute = Integer.toString(minute);
-        String s_hour = Integer.toString(hour);
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
 
-        if (hour == 0) {
-            s_hour = Integer.toString(12);
-        } else if (hour > 12) {
-            hour = hour - 12;
-            s_hour = Integer.toString(hour);
-            am_pm = "PM";
-        }
-        if (hour < 10 && hour != 0) {
-            s_hour = "0" + hour;
-        }
-        if (minute < 10) {
-            s_minute = "0" + minute;
-        }
-        return s_hour + ":" + s_minute + " " + am_pm;
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        Log.w(LOG_TAG, "Nothing selection in the dropdown, How???");
     }
 
     /**
      * For processing the time picker result
-     * @param hour Hour
-     * @param minute Minute
+     * @param textViewToSet The text view in which time must be set
+     * @param time Time
      */
-    public void processTimePickerResult(int hour, int minute) {
-        Log.d(LOG_TAG, "Selected start time as " + hour + ":" + minute);
-        startTimeValue.setText(getTimeIn12Hours(hour, minute));
+    protected void processTimePickerResult(TextView textViewToSet, String time) {
+        Log.d(LOG_TAG, "Selected start time as " + time);
+        textViewToSet.setText(time);
     }
 
-    public void setEndTime(View view) {
-        DialogFragment endTimeFragment = new TimePickerFragment();
-        endTimeFragment.show(getSupportFragmentManager(), TIME_PICKER_DIALOG_TAG);
-    }
 }
---
+++ app/src/main/java/com/bkprojects/soundsleep/TimePickerUtil.java
@@ -0,0 +1,71 @@
+package com.bkprojects.soundsleep;
+
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.widget.TimePicker;
+import java.util.Calendar;
+
+/**
+ * This simple Util is wrapper for time picker
+ * It has an interface to provide a callback
+ */
+public class TimePickerUtil {
+    private static final String TIME_PICKER_DIALOG_TAG = "Select Time";
+    TimePickerDialog mTimePickerDialog;
+
+    /**
+     * Interface to be used as a callback to the calling function
+     */
+    public interface onTimeSet {
+        void onTime(TimePicker view, String time);
+    }
+    onTimeSet mOnTimeSet;
+
+    public void setTimeListener(onTimeSet onTimeset) {
+        mOnTimeSet = onTimeset;
+    }
+
+    public TimePickerUtil(Context ctx) {
+        Calendar cldr = Calendar.getInstance();
+        int hour = cldr.get(Calendar.HOUR_OF_DAY);
+        int minutes = cldr.get(Calendar.MINUTE);
+        // time picker dialog
+        mTimePickerDialog = new TimePickerDialog(ctx,
+                (timePicker, hr, min) -> {
+                    String time = getTimeIn12Hours(hr, min);
+                    mOnTimeSet.onTime(timePicker, time);
+                }, hour, minutes, false);
+        mTimePickerDialog.setTitle(TIME_PICKER_DIALOG_TAG);
+        mTimePickerDialog.show();
+    }
+    public void show() {
+        mTimePickerDialog.show();
+    }
+
+    /**
+     * Converts the out time of date picker to a readable time
+     * @param hour Hour
+     * @param minute Minute
+     * @return String
+     */
+    public static String getTimeIn12Hours(int hour, int minute) {
+        String am_pm = "AM";
+        String s_minute = Integer.toString(minute);
+        String s_hour = Integer.toString(hour);
+
+        if (hour == 0) {
+            s_hour = Integer.toString(12);
+        } else if (hour > 12) {
+            hour = hour - 12;
+            s_hour = Integer.toString(hour);
+            am_pm = "PM";
+        }
+        if (hour < 10 && hour != 0) {
+            s_hour = "0" + hour;
+        }
+        if (minute < 10) {
+            s_minute = "0" + minute;
+        }
+        return s_hour + ":" + s_minute + " " + am_pm;
+    }
+}
