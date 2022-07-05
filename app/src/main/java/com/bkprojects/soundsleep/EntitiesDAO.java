45ad7b530a07b4a3482e9c079019b563249b6397
Braj K <braj.120@gmail.com>
2022-07-05 20:28:06 +0530

    Added Persistence Part 1

---
+++ app/src/main/java/com/bkprojects/soundsleep/EntitiesDAO.java
@@ -0,0 +1,70 @@
+package com.bkprojects.soundsleep;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKey;
+
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+/**
+ * Entities DAO used to persist all values as KV pairs in SharedPreference
+ */
+public class EntitiesDAO {
+
+    private static final String PREF_FILE_NAME = "shared_pref";
+    private final String START_TIME_KEY= "start_time";
+    private final String END_TIME_KEY= "end_time";
+    private final String NOTIFICATION_KEY = "notification";
+    private final String MODE_KEY = "mode";
+    private SharedPreferences sharedPreferences;
+
+    public EntitiesDAO(Context context) throws GeneralSecurityException, IOException {
+        MasterKey mkey = new MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+                .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                .build();
+        sharedPreferences =
+                EncryptedSharedPreferences.create(
+                        context,
+                        PREF_FILE_NAME,
+                        mkey,
+                        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+                );
+    }
+
+    public void setSharedPreferences(SharedPreferences sharedPreferences) {
+        this.sharedPreferences = sharedPreferences;
+    }
+
+    /**
+     * Used to save all KV pair as preferences from entities into shared preference
+     * @param entities The Entities object
+     */
+    public void savePreferences(Entities entities) {
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(START_TIME_KEY, entities.getStartTime());
+        editor.putString(END_TIME_KEY, entities.getEndTime());
+        editor.putString(MODE_KEY, entities.getMode());
+        editor.putBoolean(NOTIFICATION_KEY, entities.isNotifications());
+        editor.apply();
+    }
+
+    /**
+     * Used for getting all the KV pairs persisted as preference
+     * @param context The context of the activity
+     * @return Entities object
+     */
+    public Entities getPreferences(Context context) {
+        Entities entities = new Entities();
+        entities.setStartTime(sharedPreferences.getString(START_TIME_KEY, context.getString(R.string.start_time_default)));
+        entities.setEndTime(sharedPreferences.getString(END_TIME_KEY, context.getString(R.string.end_time_default)));
+        entities.setMode(sharedPreferences.getString(MODE_KEY, context.getString(R.string.vibrate_mode)));
+        entities.setNotifications(sharedPreferences.getBoolean(NOTIFICATION_KEY, false));
+        return entities;
+    }
+}
--- app/src/main/java/com/bkprojects/soundsleep/MainActivity.java
+++ app/src/main/java/com/bkprojects/soundsleep/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -71,10 +73,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         // On selecting a spinner item
-        String item = parent.getItemAtPosition(position).toString();
-
-        // Showing selected spinner item
-        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -92,4 +90,44 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         textViewToSet.setText(time);
     }
 
+    public void save(View view) {
+        try {
+            String startTime, endTime, mode;
+            boolean notifications;
+            startTime = ((Button)findViewById(R.id.select_start_time_btn)).getText().toString();
+            endTime = ((Button)findViewById(R.id.select_end_time_btn)).getText().toString();
+            if (this.getString(R.string.start_time_default).equals(startTime)) {
+                Toast.makeText(this, "Please select a start time", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (this.getString(R.string.end_time_default).equals(endTime)) {
+                Toast.makeText(this, "Please select a end time", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            notifications = ((SwitchMaterial)findViewById(R.id.notification_switch)).isChecked();
+            Spinner modeSpinner = findViewById(R.id.mode_select_spinner);
+            mode = modeSpinner.getSelectedItem().toString();
+            //TODO change this log statement
+            Log.i(LOG_TAG, startTime + " " +endTime + " "+notifications + " " + mode);
+
+            //Set the Entities object
+            entities.setStartTime(startTime);
+            entities.setEndTime(endTime);
+            entities.setNotifications(notifications);
+            entities.setMode(mode);
+
+            //Persist the Entities
+            EntitiesDAO entitiesDAO = new EntitiesDAO(this);
+            entitiesDAO.savePreferences(entities);
+
+            //Start the timer
+        } catch (Exception ex) {
+            Log.e(LOG_TAG, "Error encountered during execution" + ex.getMessage());
+            Toast.makeText(this, "Some error occurred while persisting the settings, Please contact App developers.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void clear(View view) {
+    }
 }
